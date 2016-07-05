79658beb90e4fad0ec103ed87507d695fb0dc1d5
Jon Rogozen <jrogozen@hotpads.com>
2016-07-04 23:12:43 -0700

    test

--- app/utils/requester.js
+++ app/utils/requester.js
@@ -1,7 +1,7 @@
 require('es6-promise').polyfill();
 import fetch from 'isomorphic-fetch'
 
-import { formatEndpoint } from 'app/utils/ApiUtils'
+// import { formatEndpoint } from 'app/utils/ApiUtils'
 
 const checkStatus = (response) => {
   if (response.status >= 200 && response.status < 300) {
@@ -20,17 +20,17 @@ const parseJSON = (response) => {
 const requester = (options) => {
   const { method, path, queryParams, body } = options
 
-  return fetch(formatEndpoint(path, queryParams), {
-    method,
-    headers: {
-      'Accept': 'application/json',
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify(body)
-  })
-    .then(checkStatus)
-    .then(parseJSON)
-    .catch((err) => err)
+  // return fetch(formatEndpoint(path, queryParams), {
+  //   method,
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-type': 'application/json'
+  //   },
+  //   body: JSON.stringify(body)
+  // })
+  //   .then(checkStatus)
+  //   .then(parseJSON)
+  //   .catch((err) => err)
 }
 
 export default requester
